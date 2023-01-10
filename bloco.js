d2ad8da5815d87ec16064811cdfa7f16dbb20d38
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-01-09 21:47:34 -0300

    Aula udemy - desafio

---
+++ bloco.js
@@ -0,0 +1,30 @@
+/* Um bloco é definido com "{" simbolo de chaves, uma maneira pra agrupar 
+sentença de codigo.
+
+pode-se usar mais de um bloco, um dentro do ouutro, exemplo:
+*/
+
+{
+    console.log('buenos dias');
+    console.log('buenos tarde');
+    console.log('buenos notches')
+ { 
+    console.log('Olá')
+    console.log('Oiii')
+   }
+ }
+
+ /* é possível criar um bloco de codigo, com sentenças vazias, 
+ como exemplo abaixo:
+ */
+
+ {
+   ;
+   ;
+   ;
+ }
+
+/* Os codigos são organizados através de Pastas, arquivos (js é do java script).
+sentenças de codigos e blocos. 
+*/
+
---
+++ comentario.js
@@ -0,0 +1,8 @@
+// console log é usado pra exibir algo na tela do pc!
+
+/* Esse é um comentario
+ * de multiplas
+linhas
+ */
+ 
+tentar NÃO comentar comentarios obvios
